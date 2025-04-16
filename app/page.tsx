e1eb3ab35a67750d17b161da3e2837d500442ce6
ChrisKnowsDev <chrisharley81@gmail.com>
2025-04-15 22:48:03 -0400

    initial jsx content for home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import Image from 'next/image';
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-7xl p-8">start here</h1>
-    </div>
+    <main>
+      <header>
+        <h3>vibeStudios</h3>
+      </header>
+      <div>
+        <div>
+          <p>
+            <strong>Welcome to vibeStudios </strong>
+            booking application
+          </p>
+          <Link href="/login">Log In</Link>
+        </div>
+        <img src="/hero.svg" alt="playing a guitar" />{' '}
+        {/**TODO: Change to component */}
+      </div>
+    </main>
   );
 }
