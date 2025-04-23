bdd56823d06f69a465b8a3e4dcef349f6b780176
ChrisKnowsDev <chrisharley81@gmail.com>
2025-04-23 08:06:06 -0400

    created type definitions for app

---
+++ app/lib/definitions.ts
@@ -0,0 +1,72 @@
+// Contains type definitions for app data
+// describes the shape of the data and what data type each property shoud accept
+// these types are generated automatically if using an ORM like prisma
+
+export type User = {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+};
+
+export type Client = {
+  id: string;
+  name: string;
+  email: string;
+};
+
+export type Booking = {
+  id: string;
+  customer_id: string;
+  amount: number;
+  status: 'pending' | 'paid';
+};
+
+export type Revenue = {
+  month: string;
+  revenue: number;
+};
+
+export type LatestBooking = {
+  id: string;
+  name: string;
+  image_url: string;
+  email: string;
+  amount: string;
+};
+
+// DB returns a number for amount, we will format it to a string with the formatCurrncy function
+export type LatestBookingRaw = Omit<LatestBooking, 'amount'> & {
+  amount: number;
+};
+
+export type BookingsTable = {
+  id: string;
+  customer_id: string;
+  name: string;
+  email: string;
+  date: string;
+  amount: number;
+  status: 'pending' | 'paid';
+};
+
+export type ClientTable = {
+  id: string;
+  name: string;
+  email: string;
+  total_bookings: number;
+  total_pending: string;
+  total_paid: string;
+};
+
+export type ClientField = {
+  id: string;
+  name: string;
+};
+
+export type BookingForm = {
+  id: string;
+  customer_id: string;
+  amount: number;
+  status: 'pending' | 'paid';
+};
