f80615eec4d8e4bba612821b864d4572a42b1c0f
ChrisKnowsDev <chrisharley81@gmail.com>
2025-04-23 07:06:53 -0400

    dashboard sidenav complete

--- app/components/dashboard/nav-links.tsx
+++ app/components/dashboard/nav-links.tsx
@@ -13,9 +13,13 @@ export default function NavLinks() {
         const LinkIcon = link.icon;
 
         return (
-          <a key={link.name} href={link.href}>
-            <LinkIcon />
-            <p>{link.name}</p>
+          <a
+            key={link.name}
+            href={link.href}
+            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-slate-100 p-3 text-sm font-medium md:flex-none hover:bg-violet-100 hover:text-violet-600  md:justify-start md:p-2"
+          >
+            <LinkIcon className="w-6" />
+            <p className="hidden md:block">{link.name}</p>
           </a>
         );
       })}
