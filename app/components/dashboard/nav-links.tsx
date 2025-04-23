24e74036bddc42329f66d2a59f1f004773d31cd1
ChrisKnowsDev <chrisharley81@gmail.com>
2025-04-23 07:25:40 -0400

    setup active link on dashboard sidenav

--- app/components/dashboard/nav-links.tsx
+++ app/components/dashboard/nav-links.tsx
@@ -1,4 +1,9 @@
+'use client';
+
 import { House, Files, Users } from 'lucide-react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
 
 const links = [
   { name: 'Home', href: '/dashboard', icon: House },
@@ -7,20 +12,28 @@ const links = [
 ];
 
 export default function NavLinks() {
+  const pathname = usePathname();
+
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
 
         return (
-          <a
+          <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-slate-100 p-3 text-sm font-medium md:flex-none hover:bg-violet-100 hover:text-violet-600  md:justify-start md:p-2"
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-slate-100 p-3 text-sm font-medium md:flex-none hover:text-violet-600  md:justify-start md:p-2',
+
+              {
+                'bg-violet-100 text-violet-600': pathname === link.href,
+              }
+            )}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
