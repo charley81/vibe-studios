cf89db6a4ea8d8b24f666130b2ab0078c5a77fb6
ChrisKnowsDev <chrisharley81@gmail.com>
2025-05-05 13:17:01 -0400

    initial jsx for dashboard page before fetching data

--- app/components/dashboard/latest-bookings.tsx
+++ app/components/dashboard/latest-bookings.tsx
@@ -1,3 +1,33 @@
-export default function LatestBookings() {
-  return <div>LatestBookings Component</div>;
+import { RefreshCw } from 'lucide-react';
+import { LatestBooking } from '@/app/lib/definitions';
+
+export default function LatestBookings({
+  latestBookings
+}: {
+  latestBookings: LatestBooking[];
+}) {
+  return (
+    <div>
+      <h2>Latest Bookings</h2>
+      <div>
+        <div>
+          {latestBookings.map((booking, i) => {
+            return (
+              <div key={booking.id}>
+                <div>
+                  <p>{booking.name}</p>
+                  <p>{booking.email}</p>
+                </div>
+                <p>{booking.amount}</p>
+              </div>
+            );
+          })}
+        </div>
+        <div>
+          <RefreshCw />
+          <h3>Updated just now</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,20 @@
+import { Card } from '../components/dashboard/cards';
+import RevenueChart from '../components/dashboard/revenue-chart';
+import LatestBookings from '../components/dashboard/latest-bookings';
+
 export default function DashboarPage() {
-  return <div>Dashboard Page</div>;
+  return (
+    <main>
+      <h1>Dashboard</h1>
+      {/* <div>
+        <Card/>
+        <Card/>
+        <Card/>
+      </div> */}
+      {/* <div>
+        <RevenueChart />
+        <LatestBookings />
+      </div> */}
+    </main>
+  );
 }
