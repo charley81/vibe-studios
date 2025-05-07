a8020d540d09e0b685a17d25eddcdb2a6ee2a048
ChrisKnowsDev <chrisharley81@gmail.com>
2025-05-07 10:43:46 -0400

    jsx and styling for search component complete

--- app/components/ui/search.tsx
+++ app/components/ui/search.tsx
@@ -1,3 +1,19 @@
-export default function Search() {
-  return <div>Search</div>;
+'use client';
+
+import { Search as SearchIcon } from 'lucide-react';
+
+export default function Search({ placeholder }: { placeholder: string }) {
+  return (
+    <div className="relative flex flex-1 flex-shrink-0">
+      <label htmlFor="search" className="sr-only">
+        Search
+      </label>
+      <input
+        type="text"
+        className="peer block w-full rounded-md border border-slate-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-slate-500"
+        placeholder={placeholder}
+      />
+      <SearchIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-slate-500 peer-focus:text-slate-900" />
+    </div>
+  );
 }
--- app/dashboard/bookings/page.tsx
+++ app/dashboard/bookings/page.tsx
@@ -1,7 +1,26 @@
 import Pagination from '@/app/components/bookings/pagination';
 import Search from '@/app/components/ui/search';
 import Table from '@/app/components/bookings/table';
+import { CreateBooking } from '@/app/components/bookings/buttons';
+import { BookingsTableSkeleton } from '@/app/components/dashboard/skeletons';
+import { Suspense } from 'react';
 
 export default function BookingsPage() {
-  return <div></div>;
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl font-bold">Bookings</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search bookings..." />
+        <CreateBooking />
+      </div>
+      {/* <Suspense key={query + currentPage} fallback={<BookingsTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense> */}
+      <div className="mt-5 flex w-full justify-center">
+        {/* <PannerNode totalPages={totalPages}/> */}
+      </div>
+    </div>
+  );
 }
